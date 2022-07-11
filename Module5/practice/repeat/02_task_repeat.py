2bbd1dadebb83aa7c1c592f0cca172ee412048a2
YolkaAkh <108610675+YolkaAkh@users.noreply.github.com>
2022-07-11 22:02:33 +0300

    Update 02_task_repeat.py
    сделала через срезы, вроде верно, но в результате пишет ответ, что число является палиндромом, но второй строкой выдает None. не пойму из-за чего...

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -5,3 +5,12 @@
 
 def palindrome(number):
     pass
+
+def palindrom(number):
+    number = str(number)
+    if number[::2] == number[::-2]:
+        print('число является палиндромом')
+    else:
+        print('число не является палиндромом')
+number = int(input('введите число '))
+print(palindrom(number))
