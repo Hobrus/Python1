630b4aafa9929564c436687b808a068da2823ca2
YolkaAkh <108610675+YolkaAkh@users.noreply.github.com>
2022-07-11 20:58:46 +0300

    Update 01_task_repeat.py
    вроде получилось написать, почему-то сложно было совместить и диапазон, и размер списка

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -5,3 +5,15 @@
 def gen_list(size, of, to):
     pass
 
+import random
+lst = []
+def gen_list(of, to, size):
+    for i in range(size):
+        lst.append(random.randint(of, to))
+    return lst
+
+a = 0
+b = 15
+c = 5
+result = gen_list(a,b,c)
+print(result)
