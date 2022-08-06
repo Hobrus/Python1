cc2367ec3e8007bc495e2a321373745e0fdc1bb9
MaksimSveshnikov <105560855+MaksimSveshnikov@users.noreply.github.com>
2022-08-06 14:20:34 +0300

    Update 01_hw.py
    И снова математика....

--- Module2/home_work/01_hw.py
+++ Module2/home_work/01_hw.py
@@ -6,3 +6,11 @@
 # Выведите «YES», если можно отломить от шоколадки ровно k долек, и «NO» иначе.
 
 # TODO: your code here
+n = int(input ('Введите число n: '))
+m = int(input ('Введите число m: '))
+k = int(input('Введите число k: '))
+
+if k < n * m and ((k % n == 0) or (k % m == 0)):
+    print("YES")
+else:
+    print("NO")
