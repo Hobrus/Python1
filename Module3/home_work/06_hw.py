3532b8ede95055b0bdea1e53d1d7bd1e6ae0610a
trds42 <trds42@gmail.com>
2022-08-13 16:29:56 +0300

    Update 06_hw.py
    Как можно найти в списке словарь по определенную условию, кроме как перебором в цикле?
    Например, с определенной ценой?

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -35,11 +35,31 @@ items = [
 print("Товары на складе представлены брэндами: ")
 
 # TODO: your code here
+brands = []
+for item in items:
+    brands.append(item["brand"])
+brands = set(brands)
+print(*brands, sep=", ")
 
 print("На складе больше всего товаров брэнда(ов): ")
 
 # TODO: your code here
+brands_count = {brand: 0 for brand in brands}
+for item in items:
+    brands_count[item['brand']] = brands_count[item['brand']] + 1
+max_count = max(brands_count.values())
+max_count_brands = [brand for brand, count in brands_count.items() if count == max_count]
+print(*max_count_brands, sep=", ")
 
 print("На складе самый дорогой товар брэнда(ов): ")
 
 # TODO: your code here
+max_price = 0
+for item in items:
+    if item['price'] > max_price:
+        max_price = item['price']
+max_price_brands = []
+for item in items:
+    if item['price'] == max_price:
+        max_price_brands.append(item['brand'])
+print(*max_price_brands, sep=", ")
