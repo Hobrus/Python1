22de4da012007045d1591aa5c36c2502b674144d
trds42 <trds42@gmail.com>
2022-08-13 14:39:09 +0300

    Update [hw]02_task_string.py
    Можно ли проще обработать первое и последнее слово (когда нет пробела до или пробела после)?

--- Module3/home_work/string/[hw]02_task_string.py
+++ Module3/home_work/string/[hw]02_task_string.py
@@ -1,3 +1,23 @@
 # Дана строка состоящая из слов, все слова разделены ровно одним пробелом. Знаки препинания отсутствуют.
 # Найдите первое слово, начинающееся на букву "б"
 # если слова на эту букву нет, выведите "слов на б нет"
+
+text = input('text: ')
+
+if text[0] == 'б':
+    end = text.find(' ')
+    if end >= 0:
+        print(text[:end])
+    else:
+        print(text)
+else:
+    find_start = text.find(' б')
+    start = find_start + 1
+    if find_start >= 0:
+        end = text.find(' ', start)
+        if end >= 0:
+            print(text[start:end])
+        else:
+            print(text[start:])
+    else:
+        print('No words with "б"')
