aaa06cf4c6cad81c765e7a8c1778b5a6c0cd6169
nminsok <110337525+nminsok@users.noreply.github.com>
2022-08-13 23:09:17 +0300

    Update 06_hw.py

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -35,11 +35,51 @@ items = [
 print("Товары на складе представлены брэндами: ")
 
 # TODO: your code here
+brands = []
+for element in items:
+    brand = element["brand"]
+    if brands.count(brand) == 0:
+        brands.append(brand)
+print(*brands, sep=", ", end=".\n")
 
 print("На складе больше всего товаров брэнда(ов): ")
 
 # TODO: your code here
+brands = []
+goods = []
+for element in items:
+    brand = element["brand"]
+    if brands.count(brand) == 0:
+        brands.append(brand)
+    goods.append(brand)
+#for brand in brands:
+#    goods.count(brand)
+#    print(goods.count(brand))
+max_brands = []
+max_goods = 0
+for brand in brands:
+    if goods.count(brand) > max_goods:
+        max_brands = [brand]
+        max_goods = goods.count(brand)
+    elif goods.count(brand) == max_goods:
+        max_brands.append(brand)
+#print(brands)
+#print(goods)
+print(*max_brands, sep=", ", end=".\n")
 
 print("На складе самый дорогой товар брэнда(ов): ")
 
 # TODO: your code here
+max_price = 0
+max_brands = []
+for element in items:
+    el_price = element["price"]
+    el_brand = element["brand"]
+    if el_price > max_price:
+        max_price = el_price
+        max_brands = [el_brand]
+    elif el_price == max_price:
+        if max_brands.count(el_brand) == 0:
+            max_brands.append(el_brand)
+
+print(*max_brands, sep=", ", end=".\n")
